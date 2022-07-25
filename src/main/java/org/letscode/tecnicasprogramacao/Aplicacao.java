561fd49e1ac6278ddf4c6e2ebb790e17f52996ee
Rafael Mendes <rafael.rrmendes@gmail.com>
2022-07-25 13:57:08 -0300

    classe obter melhores filmes de terror

--- src/main/java/org/letscode/tecnicasprogramacao/Aplicacao.java
+++ src/main/java/org/letscode/tecnicasprogramacao/Aplicacao.java
@@ -4,10 +4,13 @@ import java.util.List;
 
 import org.letscode.tecnicasprogramacao.model.Filme;
 import org.letscode.tecnicasprogramacao.services.CarregarFilmes;
+import org.letscode.tecnicasprogramacao.services.ObterVinteMelhoresFilmesTerror;
 
 public class Aplicacao {
     public static void main(String[] args) {
         CarregarFilmes carregarFilmes = new CarregarFilmes();
         List<Filme> filmes = carregarFilmes.executar();
+        ObterVinteMelhoresFilmesTerror obterVinteMelhoresFilmesTerror = new ObterVinteMelhoresFilmesTerror();
+        List<Filme> melhoresFilmesTerror = obterVinteMelhoresFilmesTerror.executar(filmes);
     }
 }
--- src/main/java/org/letscode/tecnicasprogramacao/services/ObterVinteMelhoresFilmesTerror.java
+++ src/main/java/org/letscode/tecnicasprogramacao/services/ObterVinteMelhoresFilmesTerror.java
@@ -1,9 +1,44 @@
 package org.letscode.tecnicasprogramacao.services;
 
+import org.letscode.tecnicasprogramacao.model.Filme;
+
+import java.util.Comparator;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
+
 public class ObterVinteMelhoresFilmesTerror {
 
-    public void executar() {
-
+    public List<Filme> executar(List<Filme> filmes) {
+        List<Filme> filmesTerror = filtrarMelhoresFilmesTerror(filmes);
+        return filmesTerror;
     }
+
+    private List<Filme> filtrarMelhoresFilmesTerror(List<Filme> filmes){
+        List<Filme> filmesTerror = filmes.stream().filter(filme -> isTerror(filme)).sorted(new Comparator<Filme>() {
+            @Override
+            public int compare(Filme o1, Filme o2) {
+                if (o1.getRank() < o2.getRank()){
+                    return -1;
+                } else if (o1.getRank() > o2.getRank()) {
+                    return 1;
+                }
+                return 0;
+            }
+        }).limit(20).collect(Collectors.toList());
+        return filmesTerror;
+    }
+
+    private boolean isTerror(Filme filme){
+        Pattern pattern = Pattern.compile(".*(?i:(Horror)).*");
+        Matcher matcher = pattern.matcher(filme.getGenre());
+
+        while(matcher.find()){
+            return true;
+        }
+        return false;
+    }
+
     
 }
