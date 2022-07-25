222f19a91c62a4b9a9bb4ac2e4e7343bf46fbbf2
Rodolfo Medeiros <sd.rodolfo.medeiros@gmail.com>
2022-07-25 14:25:20 -0300

    refactor: utiliza lambda, stream e programaçao funcional no carregamento dos arquivos/filmes

--- src/main/java/org/letscode/tecnicasprogramacao/services/CarregarFilmes.java
+++ src/main/java/org/letscode/tecnicasprogramacao/services/CarregarFilmes.java
@@ -4,6 +4,8 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 import org.letscode.tecnicasprogramacao.core.FileReader;
 import org.letscode.tecnicasprogramacao.model.Filme;
@@ -14,33 +16,32 @@ public class CarregarFilmes {
         Pattern pattern = Pattern.compile("(?:,|\\n|^)(\"(?:(?:\"\")*[^\"]*)*\"|[^\",\n]*|(?:\n|$))");
         Matcher matcher = pattern.matcher(fileline);
         List<String> campos = new ArrayList<>();
-        
+
         while (matcher.find()) {
             campos.add(matcher.group(1));
         }
 
-        Filme filme = new Filme(
-            !campos.get(0).isEmpty() ? Long.parseLong(campos.get(0)) : null,
-            campos.get(1),
-            campos.get(2),
-            campos.get(3),
-            campos.get(4),
-            campos.get(5),
-            !campos.get(6).isEmpty() ? Long.parseLong(campos.get(6)) : null,
-            !campos.get(7).isEmpty() ? Long.parseLong(campos.get(7)) : null,
-            !campos.get(8).isEmpty() ? Double.parseDouble(campos.get(8)) : null,
-            !campos.get(9).isEmpty() ? Long.parseLong(campos.get(9)) : null,
-            !campos.get(10).isEmpty() ? Double.parseDouble(campos.get(10)) : null,
-            !campos.get(11).isEmpty() ? Long.parseLong(campos.get(11)) : null
-        );
-        return filme;
+        return Filme.builder()
+                .Rank(!campos.get(0).isEmpty() ? Long.parseLong(campos.get(0)) : null)
+                .Title(campos.get(1))
+                .Genre(campos.get(2))
+                .Description(campos.get(3))
+                .Director(campos.get(4))
+                .Actors(campos.get(5))
+                .Year(!campos.get(6).isEmpty() ? Long.parseLong(campos.get(6)) : null)
+                .Runtime(!campos.get(7).isEmpty() ? Long.parseLong(campos.get(7)) : null)
+                .Rating(!campos.get(8).isEmpty() ? Double.parseDouble(campos.get(8)) : null)
+                .Votes(!campos.get(9).isEmpty() ? Long.parseLong(campos.get(9)) : null)
+                .Revenue(!campos.get(10).isEmpty() ? Double.parseDouble(campos.get(10)) : null)
+                .Metascore(!campos.get(11).isEmpty() ? Long.parseLong(campos.get(11)) : null)
+                .build();
     }
 
     public List<Filme> executar() {
         System.out.println("Carregando filmes...");
 
         List<Filme> filmes = new ArrayList<>();
-        
+
         //Array de threads de leitura
         List<FileReader> threads = new ArrayList<>();
 
@@ -50,28 +51,25 @@ public class CarregarFilmes {
         threads.add(new FileReader("src/main/resources/movies3.csv"));
 
         //Inicia as threads
-        for (FileReader thread : threads) {
-            thread.start();
-        }
-
-        List<String> linhas = new ArrayList<>();
-
-        //Espera todas as threads terminarem
-        for(FileReader thread : threads) {
-            try {
-                thread.join();
-                linhas.addAll(thread.getLines());
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-        }
-
-        //Para cada linha dos arquivos, cria um filme e adiciona a lista de filmes
-        for (String linha : linhas) {
-            filmes.add(parseFilme(linha));
-        }
-
-        return filmes;
+        return threads.parallelStream()
+                .map(thread -> {
+                    thread.run();
+                    try {
+                        //Espera todas as threads terminarem
+                        thread.join();
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                    return Stream.of(thread.getLines());
+                })
+                .flatMap(stream -> stream.distinct())
+                .flatMap(lista -> lista.stream())
+                .distinct()
+                //Para cada linha dos arquivos, cria um filme e adiciona a lista de filmes
+                .map(linha -> parseFilme(linha))
+                // Ordena os filmes
+                .sorted((f1, f2) -> Long.compare(f1.getRank(),f2.getRank()))
+                .collect(Collectors.toList());
     }
 
 }
