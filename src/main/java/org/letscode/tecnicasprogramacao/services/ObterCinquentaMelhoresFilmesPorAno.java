2f9fe795e859ccdd2fe397d3a24a8c8bdb8b497c
william-brito <will.oliver.ads@gmail.com>
2022-07-26 17:16:30 -0300

    Obter cinquenta melhores filmes por ano

--- src/main/java/org/letscode/tecnicasprogramacao/services/ObterCinquentaMelhoresFilmesPorAno.java
+++ src/main/java/org/letscode/tecnicasprogramacao/services/ObterCinquentaMelhoresFilmesPorAno.java
@@ -8,7 +8,30 @@ import java.util.List;
 public class ObterCinquentaMelhoresFilmesPorAno implements Processamento<Filme> {
 
     @Override
-    public List<Filme> executar(List<Filme> filmes) {
-        return null;
+    public HashMap<List<Filme>, Long> executar(List<Filme> filmes, Long ano) {
+        
+        HashMap<List<Filme>, Long> melhoresFilmes = new HashMap<>();
+
+        for (Long ano : filmes.stream().parallelStream().map(x -> x.getYear()).distinct().collect(Collections.toList()) ) {
+            melhoresFilmes.put(filtrarMelhoresFilmes(), ano)
+        }
+
+        return melhoresFilmes;
     }
+
+    private List<Filme> filtrarMelhoresFilmes(List<Filme> filmes, Long ano){
+        List<Filme> MelhoresFilmes = filmes.stream().filter(x -> x.getYear == ano).sorted(new Comparator<Filme>() {
+            @Override
+            public int compare(Filme o1, Filme o2) {
+                if (o1.getRank() < o2.getRank()){
+                    return -1;
+                } else if (o1.getRank() > o2.getRank()) {
+                    return 1;
+                }
+                return 0;
+            }
+        }).limit(50).collect(Collectors.toList());
+        return MelhoresFilmes;
+    }
+
 }
