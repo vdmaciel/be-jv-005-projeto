9321cd7a2c53139734ec7698566981b1cc759878
Rodolfo Medeiros <sd.rodolfo.medeiros@gmail.com>
2022-07-26 15:35:53 -0300

    refactor: usa Future, ExecutorService e Stream

--- src/main/java/org/letscode/tecnicasprogramacao/Aplicacao.java
+++ src/main/java/org/letscode/tecnicasprogramacao/Aplicacao.java
@@ -1,5 +1,7 @@
 package org.letscode.tecnicasprogramacao;
 
+import java.nio.file.Path;
+import java.util.Arrays;
 import java.util.List;
 
 import org.letscode.tecnicasprogramacao.model.Filme;
@@ -7,10 +9,18 @@ import org.letscode.tecnicasprogramacao.services.CarregarFilmes;
 import org.letscode.tecnicasprogramacao.services.ObterVinteMelhoresFilmesTerror;
 
 public class Aplicacao {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
+
+        Path[] entradaArquivos = {
+                Path.of("src/main/resources/movies1.csv"),
+                Path.of("src/main/resources/movies2.csv"),
+                Path.of("src/main/resources/movies3.csv")
+        };
 
         CarregarFilmes carregarFilmes = new CarregarFilmes();
-        List<Filme> filmes = carregarFilmes.executar();
+        List<Filme> filmes = carregarFilmes.executar(Arrays.asList(entradaArquivos));
+
+        filmes.stream().sequential().forEach(System.out::println);
 
         /* TODO usar Strategy para executar todos os serviços (/services) usando interface Processamento;
             - Criar uma lista de todos os processamentos a executar
