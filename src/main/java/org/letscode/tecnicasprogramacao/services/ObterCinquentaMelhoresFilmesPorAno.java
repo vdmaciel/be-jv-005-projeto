8daf4a13b22a0ea7f3074e3df370a4009450fe6f
F9513636 Victor Daniel Vieira Maciel <victormaciel0216@gmail.com>
2022-07-26 17:53:38 -0300

    escreve arquivos com ranking de filmes por  ano

--- src/main/java/org/letscode/tecnicasprogramacao/services/ObterCinquentaMelhoresFilmesPorAno.java
+++ src/main/java/org/letscode/tecnicasprogramacao/services/ObterCinquentaMelhoresFilmesPorAno.java
@@ -1,26 +1,31 @@
 package org.letscode.tecnicasprogramacao.services;
 
+import org.letscode.tecnicasprogramacao.core.FileWrite;
 import org.letscode.tecnicasprogramacao.model.Filme;
 import org.letscode.tecnicasprogramacao.model.Processamento;
 
+import java.util.Comparator;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class ObterCinquentaMelhoresFilmesPorAno implements Processamento<Filme> {
 
     @Override
-    public HashMap<List<Filme>, Long> executar(List<Filme> filmes, Long ano) {
-        
-        HashMap<List<Filme>, Long> melhoresFilmes = new HashMap<>();
+    public void executar(List<Filme> filmes) {
 
-        for (Long ano : filmes.stream().parallelStream().map(x -> x.getYear()).distinct().collect(Collections.toList()) ) {
-            melhoresFilmes.put(filtrarMelhoresFilmes(), ano)
+        for (Long ano : filmes.stream().parallel().map(x -> x.getYear()).distinct().collect(Collectors.toList()) ) {
+            FileWrite writer = new FileWrite("src/main/resources/melhores_filmes_"+ ano +".txt");
+            writer.setLines(filtrarMelhoresFilmes(filmes, ano)
+                .parallelStream()
+                .map(f -> f.toString())
+                .collect(Collectors.toList()));
+            writer.run(null);
         }
 
-        return melhoresFilmes;
     }
 
     private List<Filme> filtrarMelhoresFilmes(List<Filme> filmes, Long ano){
-        List<Filme> MelhoresFilmes = filmes.stream().filter(x -> x.getYear == ano).sorted(new Comparator<Filme>() {
+        List<Filme> MelhoresFilmes = filmes.stream().filter(x -> x.getYear().equals(ano)).sorted(new Comparator<Filme>() {
             @Override
             public int compare(Filme o1, Filme o2) {
                 if (o1.getRank() < o2.getRank()){
